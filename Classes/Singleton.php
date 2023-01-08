d4607f7e9a442398386ff3b4069253306b4f5e1e
Andrei <andreivirgil@hotmail.com>
2023-01-08 19:42:39 +0200

    Update pt Heroku

--- Classes/Singleton.php
+++ Classes/Singleton.php
@@ -9,10 +9,10 @@ class Singleton
     {
         if(empty(self::$instance))
         {
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "shop";
+            $servername = "eu-cdbr-west-03.cleardb.net";
+            $username = "b2eb4acfda2994";
+            $password = "03067a51";
+            $dbname = "heroku_b696645800b15fd";
             self::$instance = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
         }
         return self::$instance;
