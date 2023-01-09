96ec0ba2b7036e285ee85e3e85a38d9584643a75
Andrei <andreivirgil@hotmail.com>
2023-01-09 15:03:22 +0200

    Acl working again

--- AdminHeader.php
+++ AdminHeader.php
@@ -7,10 +7,10 @@ $currentPage = explode(".", explode("/", $url)[1])[0];
 var_dump($currentPage);
 $role = $_SESSION['userType'];
 $numRows = DbOperations::numQueryResults("SELECT * FROM sectionsroles WHERE (IDRole = $role AND IDSection = (SELECT ID FROM SECTIONS WHERE SectionName = '$currentPage'))");
-//if($numRows == 0)
-//{
-//    header("Location: Error403.html");
-//}
+if($numRows == 0)
+{
+    header("Location: Error403.html");
+}
 
 ?>
 <!doctype html>
