b5defb4ca2323c04805e821db94f0ac885d0a33e
Andrei <andreivirgil@hotmail.com>
2023-01-10 19:48:19 +0200

    Changed email address

--- Buyers/SentOrder.php
+++ Buyers/SentOrder.php
@@ -7,7 +7,7 @@ if($_SESSION['userType'] != 4)
 }
 
 use PHPMailer\PHPMailer\PHPMailer;
-
+require 'DbOperations.php';
 require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
@@ -15,68 +15,65 @@ require('../fpdf185/fpdf.php');
 $pdf = new FPDF('P','mm','A4');
 $pdf->AddPage();
 
-//set font to arial, bold, 14pt
 $pdf->SetFont('Arial','B',14);
 
-//Cell(width , height , text , border , end line , [align] )
-
 $pdf->Cell(130 ,5,'La jumate',0,0);
-$pdf->Cell(59 ,5,'INVOICE',0,1);//end of line
+$pdf->Cell(59 ,5,'INVOICE',0,1);
 
 //set font to arial, regular, 12pt
 $pdf->SetFont('Arial','',12);
 
 $pdf->Cell(130 ,5,'BD. Regina Elisabeta',0,0);
-$pdf->Cell(59 ,5,'',0,1);//end of line
+$pdf->Cell(59 ,5,'',0,1);
 
 $pdf->Cell(130 ,5,'Bucuresti, Romania, 030018',0,0);
 $pdf->Cell(25 ,5,'Date',0,0);
-$pdf->Cell(34 ,5,'13/01/2023',0,1);//end of line
+$pdf->Cell(34 ,5,'13/01/2023',0,1);
 
 $pdf->Cell(130 ,5,'Phone 0123456789',0,0);
 $pdf->Cell(25 ,5,'Invoice #',0,0);
-$pdf->Cell(34 ,5,'307',0,1);//end of line
+$pdf->Cell(34 ,5,'307',0,1);
 
 $pdf->Cell(130 ,5,'Fax 1234567',0,0);
 $pdf->Cell(25 ,5,'Customer ID',0,0);
-$pdf->Cell(34 ,5,'51',0,1);//end of line
+$pdf->Cell(34 ,5,'51',0,1);
 
-//make a dummy empty cell as a vertical spacer
-$pdf->Cell(189 ,10,'',0,1);//end of line
+$pdf->Cell(189 ,10,'',0,1);
 
-//make a dummy empty cell as a vertical spacer
-$pdf->Cell(189 ,10,'',0,1);//end of line
+$pdf->Cell(189 ,10,'',0,1);
 
-//invoice contents
 $pdf->SetFont('Arial','B',12);
 
 $pdf->Cell(130 ,5,'Product',1,0);
 $pdf->Cell(25 ,5,'Quantity',1,0);
-$pdf->Cell(34 ,5,'Amount',1,1);//end of line
+$pdf->Cell(34 ,5,'Amount',1,1);
 
 $pdf->SetFont('Arial','',12);
-
-//Numbers are right-aligned so we give 'R' after new line parameter
 
 for($i = 0; $i < $_SESSION['numberOfProducts']; ++$i)
 {
     $pdf->Cell(130 ,5, $_SESSION['products'][$i * 5],1,0);
     $pdf->Cell(25 ,5,  $_SESSION['quantity'][$i],1,0);
-    $pdf->Cell(34 ,5,  $_SESSION['products'][$i * 5 + 2] * $_SESSION['quantity'][$i],1,1,'R');//end of line
+    $pdf->Cell(34 ,5,  $_SESSION['products'][$i * 5 + 2] * $_SESSION['quantity'][$i],1,1,'R');
+
+    $newQuantity = $_SESSION['products'][$i * 5 + 4] - $_SESSION['quantity'];
+    $givenID = $_SESSION['products'][$i * 5];
+    DbOperations::insertIntoDB("UPDATE foods SET Quantity = '$newQuantity' WHERE ID = $givenID;");
+    DbOperations::insertIntoDB("UPDATE toys SET Quantity = '$newQuantity' WHERE ID = $givenID;");
+    DbOperations::insertIntoDB("UPDATE electronics SET Quantity = '$newQuantity' WHERE ID = $givenID;");
 }
 
-//summary
 $pdf->Cell(130 ,5,'',0,0);
 $pdf->Cell(25 ,5,'Subtotal',0,0);
-$pdf->Cell(34 ,5,$_SESSION['total'] ,1,1,'R');//end of line
+$pdf->Cell(34 ,5,$_SESSION['total'] ,1,1,'R');
 
 $pdf->Cell(130 ,5,'',0,0);
 $pdf->Cell(25 ,5,'Tva',0,0);
-$pdf->Cell(34 ,5,'19%',1,1,'R');//end of line
+$pdf->Cell(34 ,5,'19%',1,1,'R');
 
 $pdf->Cell(130 ,5,'',0,0);
 $pdf->Cell(25 ,5,'Total Due',0,0);
-$pdf->Cell(34 ,5,$_SESSION['total'],1,1,'R');//end of line
+$pdf->Cell(34 ,5,$_SESSION['total'],1,1,'R');
 
 //$mail = new PHPMailer(1);
 //$mail->isSMTP();
