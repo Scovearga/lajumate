48121e3cc32fa6b329544b02f7d05a9b09ddb3af
Andrei <andreivirgil@hotmail.com>
2023-01-11 22:11:40 +0200

    Added stats

--- Statistics.php
+++ Statistics.php
@@ -44,12 +44,20 @@ echo "<h2>Vizitatori pagina Shop: " . getShopViews() . "</h2>";
 echo "<h2>Vizitatori pagina Cart: " . getCartViews() . "</h2>";
 echo "<h2>Vizitatori pagina Sent Order: " . getSentOrderViews() . "</h2>";
 
+$totalPageViews = getShopViews() + getCartViews() + getSentOrderViews();
+
 $dataPoints = array(
 array("label"=>"Windows", "y"=> (100 * getWindowsUsers()) / getTotalUsers()),
 array("label"=>"Android", "y"=> (100 * getAndroidUsers()) / getTotalUsers()),
 array("label"=>"IOS", "y"=> (100 * getIosUsers()) / getTotalUsers()),
 array("label"=>"MAC", "y"=> (100 * getMacUsers()) / getTotalUsers()),
-)
+);
+
+$dataPoints2 = array(
+    array("label"=>"Pagina Shop", "y"=> (100 * getShopViews()) / $totalPageViews),
+    array("label"=>"Pagina Cart", "y"=> (100 * getCartViews()) / $totalPageViews),
+    array("label"=>"Pagina Sent Order", "y"=> (100 * getSentOrderViews()) / $totalPageViews)
+);
 
 ?>
 <!DOCTYPE HTML>
@@ -70,12 +78,27 @@ array("label"=>"MAC", "y"=> (100 * getMacUsers()) / getTotalUsers()),
                 }]
             });
             chart.render();
-
+        }
+        window.onload = function() {
+            var chart = new CanvasJS.Chart("chartContainer2", {
+                animationEnabled: true,
+                title: {
+                    text: "Procentaj Useri in functie de Paginile Accesate"
+                },
+                data: [{
+                    type: "pie",
+                    yValueFormatString: "#,##0.00\"%\"",
+                    indexLabel: "{label} ({y})",
+                    dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render();
         }
     </script>
 </head>
 <body>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="chartContainer2" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>
